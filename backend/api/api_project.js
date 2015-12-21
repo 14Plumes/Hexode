3cd8e0370bd3a922d3fb33370d48ea043587c327
Alfred <travis@testing-worker-linux-docker-aa95f853-3188-linux-7.prod.travis-ci.org>
2015-12-21 09:12:56 +0000

    Travis Reports build#28

--- backend/api/api_project.js
+++ backend/api/api_project.js
@@ -10,7 +10,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-12-20T23:34:25.498Z",
+    "time": "2015-12-21T09:12:54.660Z",
     "url": "http://apidocjs.com",
     "version": "0.14.0"
   }
